0efa16ff54931506dc5f4e71c3d5a3b6c49f6c68
Nicholas Costa <nicholas.philpott.costa@gmail.com>
2015-05-30 12:44:49 -0400

    YEAH SAVAED

--- control.js
+++ control.js
@@ -1,1 +1,1 @@
-
+/*jshint globals: false, unused: false, strict: false, debug: true, globalstrict: true, moz: true, browser: true, devel: true, jquery: true*/
--- events.js
+++ events.js
@@ -1,1 +1,8 @@
-
+/*jshint globals: false, unused: false, strict: false, debug: true, globalstrict: true, moz: true, browser: true, devel: true, jquery: true*/
+function initiate() {
+var canva = document.getElementById("canvas");
+var canvas = canva.getContext("2d");
+canvas.fillStyle = "red";
+canvas.fillRect(10, 10, 100, 100);
+};
+addEventListener('load', initiate)

2d3835b2d64f05b696c96464d6162c971dcb1d99
Nicholas Costa <nicholas.philpott.costa@gmail.com>
2015-05-30 17:35:11 -0400

    Needs math.

--- game.js
+++ game.js
@@ -1,69 +1,127 @@
 /*jshint globals: false, unused: false, strict: false, debug: true, globalstrict: true, moz: true, browser: true, devel: true, jquery: true*/
 var bolides = {
     canvas: {
-        ctx: ''
+        ctx: '',
     },
-    goodShip: {
-        x: 80,
-        y: 60,
+    // Attributes of the player's ship declared
+    spaceship: {
+        // Starting x and y
+        x: 200,
+        y: 140,
+        // Starting speed
         speed: 0,
+        // Starting angle
+        angle: 0,
+        // Starting direction
+        direction: {
+            x: 0,
+            y: 0
+        },
+        // Starting health
         hearts: 3
     },
+    // Attributes of the asteroids declared
     asteriod: {
-        x: 400,
-        y: 400,
-        speed: 15
+        // Same attributes as the ship, aside from height
+        x: Math.floor(Math.random() * 100 + 800),
+        y: Math.floor(Math.random() * 100 + 30),
+        speed: 5
     },
+    // Images used by the project are created here
     images: {
         ship: document.createElement('img'),
         asteroid: document.createElement('img'),
         heart: document.createElement('img')
     },
     initiate: function(){
+        // Declare the canvas's context as 2D
         var canva = document.getElementById('canvas');
         bolides.canvas.ctx = canva.getContext('2d');
+        // EventListener for keypresses to change speed and stuff
         addEventListener('keydown', function(e){bolides.control(e);});
+        // Set the image sources
         bolides.images.ship.setAttribute('src', "../bolides/images/spaceship.png");
         bolides.images.asteroid.setAttribute('src', "../bolides/images/asteroid.png");
         bolides.images.heart.setAttribute('src', "../bolides/images/heart.png");
+        // Start looping
         bolides.loop();
     },
     loop: function() {
-        if(bolides.goodShip.hearts === 0) {
+        // Is the player out of health?
+        if(bolides.spaceship.hearts === 0) {
+            // Then display "Game over"
             bolides.canvas.ctx.clearRect(0, 0, 800, 600);
-            bolides.canvas.ctx.font = "48px 'Comic Sans MS'";
+            bolides.canvas.ctx.font = "48px Ubuntu";
             bolides.canvas.ctx.fillStyle = "white";
-            bolides.canvas.ctx.fillText("U SUK", 200, 200);
+            bolides.canvas.ctx.fillText("Game Over", 200, 200);
         } else {
+            // No? Then move and draw everything, then loop again.
             bolides.move();
             bolides.draw();
             requestAnimationFrame(function() { bolides.loop(); });
         }
     },
     control: function(key) {
+        // Up key?
         if (key.keyCode === 38){
-            bolides.goodShip.speed -= 1;
+            // Then increase speed.
+            bolides.spaceship.speed += 1;
+        // Down key?
         } else if (key.keyCode === 40) {
-            bolides.goodShip.speed += 1;
+            // Then decrease speed.
+            bolides.spaceship.speed -= 1;
+        // R key?
         } else if (key.keyCode === 82) {
-            bolides.goodShip.hearts -= 1;
+            // Then remove a heart.
+            bolides.spaceship.hearts -= 1;
+        // Left key?
+        } else if (key.keyCode === 37) {
+            // Then change its angle by 5 degrees
+            bolides.spaceship.angle -= 0.08726646259971647;
+        // Right key?
+        } else if (key.keyCode === 39) {
+            // Then change its angle by -5 degrees
+            bolides.spaceship.angle += 0.08726646259971647;
         }
     },
     move: function() {
-//        bolides.goodShip.x += bolides.goodShip.speed;
-        bolides.goodShip.y += bolides.goodShip.speed;
-        bolides.asteriod.x = Math.floor(Math.random() * 500);
-        bolides.asteriod.y = Math.floor(Math.random() * 500);
+        // Complicated mathy things I do not understand
+        bolides.spaceship.direction.x = Math.sin(bolides.spaceship.angle);
+        bolides.spaceship.direction.y = Math.cos(bolides.spaceship.angle);
+        bolides.spaceship.x += bolides.spaceship.direction.x * bolides.spaceship.speed;
+        bolides.spaceship.y += bolides.spaceship.direction.y * bolides.spaceship.speed;
+        // If it's moving,
+        if (bolides.spaceship.speed !== 0) {
+            // Use the moving ship pic.
+            bolides.images.ship.setAttribute('src', "../bolides/images/spaceship-move.png");
+        // Elsewise, don't.
+        } else {
+            bolides.images.ship.setAttribute('src', "../bolides/images/spaceship.png");
+        }
+//        bolides.asteriod.x = ;
+//        bolides.asteriod.y = 650;
     },
     draw: function() {
+        // Clear the canvas
         bolides.canvas.ctx.clearRect(0, 0, 800, 600);
-        bolides.canvas.ctx.drawImage(bolides.images.ship, bolides.goodShip.x, bolides.goodShip.y);
+        // Save it so I can f*** about as much as I want
+        bolides.canvas.ctx.save();
+        // Set the origin to the ship's center
+        bolides.canvas.ctx.translate(bolides.spaceship.x + 18, bolides.spaceship.y - 31);
+        // Rotate the ship around the center by the angle of the ship
+        bolides.canvas.ctx.rotate(bolides.spaceship.angle);
+        // Draw the ship
+        bolides.canvas.ctx.drawImage(bolides.images.ship, -18, -31, 36, 62);
+        // Restore all f***ing about
+        bolides.canvas.ctx.restore();
+        // Draw the asteroid (doesn't turn up because it's off the screen)
         bolides.canvas.ctx.drawImage(bolides.images.asteroid, bolides.asteriod.x, bolides.asteriod.y);
-        if (bolides.goodShip.hearts === 3) {
+        // Check for the number of hearts and draw that many
+        if (bolides.spaceship.hearts >= 3) {
             bolides.canvas.ctx.drawImage(bolides.images.heart, 10, 10);
             bolides.canvas.ctx.drawImage(bolides.images.heart, 35, 10);
             bolides.canvas.ctx.drawImage(bolides.images.heart, 60, 10);
-        } else if (bolides.goodShip.hearts === 2) {
+        } else if (bolides.spaceship.hearts === 2) {
             bolides.canvas.ctx.drawImage(bolides.images.heart, 10, 10);
             bolides.canvas.ctx.drawImage(bolides.images.heart, 35, 10);
         } else {
@@ -71,4 +129,5 @@ var bolides = {
         }
     }
 };
+// Actually start the program
 addEventListener('load', function() {bolides.initiate();});

0daad5b22198d6e74717d5dbe903f09a77969294
Aloup Shmederheisen <Aardwolf95@users.noreply.github.com>
2015-11-18 21:19:33 -0500

    Update bolides.js

--- bolides.js
+++ bolides.js
@@ -417,6 +417,7 @@ var bolides = {
     }
     if (bolides.keyPresses.right || bolides.keyPresses.d) {
       // Then change its angle by -20 degrees over 1/10 second
+      //blaze it
       var rightInterval = setInterval(function() {
         bolides.spaceship.angle += degreesToRadians(6);
       }, 20);
